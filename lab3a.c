cd836c0756b2a058f8d8cefa4bf32f21f0ad1fa8
Michael Huo <huom7473@g.ucla.edu>
2020-12-01 01:10:57 -0800

    added free inode/block scanning

--- lab3a.c
+++ lab3a.c
@@ -15,12 +15,20 @@ typedef struct ext2_super_block super_block;
 typedef struct ext2_group_desc group_desc;
 
 super_block* sb; //access super block globally
-group_desc* group;
+group_desc* group; //assume we only need to deal with single groups
 int fd;
 
+unsigned long calculate_offset(unsigned block_num) {
+    return block_num * (EXT2_MIN_BLOCK_SIZE << sb->s_log_block_size);
+}
 
-void read_super_block(){
+void read_super_block() {
     sb = malloc(sizeof(super_block));
+    if (!sb) {
+        fprintf(stderr, "%s: error in buffer allocation\n", argv[0]);
+        exit(2);
+    }
+
     pread(fd, sb, sizeof(super_block), 1024); //read super block
 
     if (sb->s_magic != EXT2_SUPER_MAGIC) {
@@ -30,13 +38,64 @@ void read_super_block(){
     
     unsigned int bsize = EXT2_MIN_BLOCK_SIZE << sb->s_log_block_size;
     fprintf(stderr, "size of block according to superblock: %d\n", bsize);
-    fprintf(stdout, "SUPERBLOCK,%d,%d,%d,%d,%d,%d,%d\n", sb->s_blocks_count, sb->s_inodes_count, bsize, sb->s_inode_size, sb->s_blocks_per_group, sb->s_inodes_per_group, sb->s_first_ino);
+    fprintf(stdout, "SUPERBLOCK,%d,%d,%d,%d,%d,%d,%d\n",
+            sb->s_blocks_count, sb->s_inodes_count, bsize, sb->s_inode_size,
+            sb->s_blocks_per_group, sb->s_inodes_per_group, sb->s_first_ino);
 }
 
-void read_group(){
+void read_group() {
     group = malloc(sizeof(group_desc));
-    pread(fd, group, sizeof(group_desc), 2048); //TODO: change this to the new offset func
-    fprintf(stdout,"GROUP,%d,%d,%d,%d,%d,%d,%d,%d\n", 0, sb->s_blocks_count, sb->s_inodes_count, group->bg_free_blocks_count, group->bg_free_inodes_count, group->bg_block_bitmap, group->bg_inode_bitmap, group->bg_inode_table);
+    if (!group) {
+        fprintf(stderr, "%s: error in buffer allocation\n", argv[0]);
+        exit(2);
+    }
+    
+    pread(fd, group, sizeof(group_desc), calculate_offset(sb->s_first_data_block + 1));
+    fprintf(stdout,"GROUP,%d,%d,%d,%d,%d,%d,%d,%d\n", 0,
+            sb->s_blocks_count, sb->s_inodes_count, group->bg_free_blocks_count,
+            group->bg_free_inodes_count, group->bg_block_bitmap, group->bg_inode_bitmap, group->bg_inode_table);
+}
+
+void scan_free_blocks() {
+    unsigned int bsize = EXT2_MIN_BLOCK_SIZE << sb->s_log_block_size;
+    unsigned char *buffer = malloc(bsize);
+    if (!buffer) {
+        fprintf(stderr, "%s: error in buffer allocation\n", argv[0]);
+        exit(2);
+    }
+
+    pread(fd, buffer, bsize, calculate_offset(group->bg_block_bitmap));
+
+    unsigned int byte, offset;
+    for(unsigned i = 0; i < sb->s_blocks_per_group; ++i) {
+        byte = i / 8; // 8 blocks represented per byte
+        offset = i % 8; //offset from that byte
+        if (!(buffer[byte] >> offset & 0x1)) { //find the right bit, if it's 0 the if statement is true
+            fprintf(stdout, "BFREE,%u\n", i + 1); //I guess block # starts at 1
+        }
+    }
+    free(buffer);
+}
+
+void scan_free_inodes() {
+    unsigned int bsize = EXT2_MIN_BLOCK_SIZE << sb->s_log_block_size;
+    unsigned char *buffer = malloc(bsize);
+    if (!buffer) {
+        fprintf(stderr, "%s: error in buffer allocation\n", argv[0]);
+        exit(2);
+    }
+
+    pread(fd, buffer, bsize, calculate_offset(group->bg_inode_bitmap));
+
+    unsigned int byte, offset;
+    for(unsigned i = 0; i < sb->s_inodes_per_group; ++i) {
+        byte = i / 8; // 8 blocks represented per byte
+        offset = i % 8; //offset from that byte
+        if (!(buffer[byte] >> offset & 0x1)) { //find the right bit, if it's 0 the if statement is true
+            fprintf(stdout, "IFREE,%u\n", i + 1); //I guess block # starts at 1
+        }
+    }
+    free(buffer);
 }
 
 int main(int argc, char **argv) {
@@ -55,5 +114,7 @@ int main(int argc, char **argv) {
     
     read_super_block();
     read_group();
+    scan_free_blocks();
+    scan_free_inodes();
     exit(0);
 }
