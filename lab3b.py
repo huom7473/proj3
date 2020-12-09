802e9881d82c55ab1c7f1530965cf36ceb58c86a
Tygan Zeng <zengtygan@gmail.com>
2020-12-08 21:34:17 -0800

    added checking for duplicate blocks, still might be buggy

--- lab3b.py
+++ lab3b.py
@@ -9,7 +9,8 @@ linesdict = {}
 
 block_free_list_set = set()
 block_allocated_set = set()
-
+block_printed_set = set()
+block_dictionary = dict()
 
 def generate_free_list():
     for i in linesdict['BFREE']:
@@ -47,6 +48,28 @@ def scan_blocks():
                         print(f"RESERVED TRIPLE INDIRECT BLOCK {i[12+j]} IN INODE {i[1]} AT OFFSET 65804")
                 else:
                     block_allocated_set.add(int(i[12+j]))
+
+                if int(i[12+j]) in block_dictionary:
+                    if j < 12:
+                        print(f"DUPLICATE BLOCK {i[12+j]} IN INODE {i[1]} AT OFFSET {j}")
+                    elif j == 12:
+                        print(f"DUPLICATE INDIRECT BLOCK {i[12+j]} IN INODE {i[1]} AT OFFSET {j}")
+                    elif j == 13:
+                        print(f"DUPLICATE DOUBLE INDIRECT BLOCK {i[12+j]} IN INODE {i[1]} AT OFFSET 268")
+                    elif j == 14:
+                        print(f"DUPLICATE TRIPLE INDIRECT BLOCK {i[12+j]} IN INODE {i[1]} AT OFFSET 65804")
+                    block_printed_set.add(int(i[12+j]))
+                elif int(i[12+j]) != 0:
+                    if j < 12:
+                        block_dictionary[int(i[12+j])] = (0, i[1], j)
+                    elif j == 12: 
+                        block_dictionary[int(i[12+j])] = (1, i[1], j)
+                    elif j == 13:
+                        block_dictionary[int(i[12+j])] = (2, i[1], 268)
+                    elif j == 14:
+                        block_dictionary[int(i[12+j])] = (3, i[1], 65804)
+                    
+                
     
     print("second loop")        
     for i in linesdict['INDIRECT']:
@@ -60,7 +83,37 @@ def scan_blocks():
                 print(f"INVALID TRIPLE INDIRECT BLOCK {i[5]} IN INODE {i[1]} AT OFFSET {i[3]}")
         else:
             block_allocated_set.add(int(i[5]))
+        
+        if int(i[5]) in block_dictionary:
+            if j < 12:
+                print(f"DUPLICATE BLOCK {i[5]} IN INODE {i[1]} AT OFFSET {i[3]}")
+            elif j == 12:
+                print(f"DUPLICATE INDIRECT BLOCK {i[5]} IN INODE {i[1]} AT OFFSET {i[3]}")
+            elif j == 13:
+                print(f"DUPLICATE DOUBLE INDIRECT BLOCK {i[5]} IN INODE {i[1]} AT OFFSET {i[3]}")
+            elif j == 14:
+                print(f"DUPLICATE TRIPLE INDIRECT BLOCK {i[5]} IN INODE {i[1]} AT OFFSET {i[3]}")
+            block_printed_set.add(int(i[5]))
+        elif int(i[5]) != 0:
+            if j < 12:
+                block_dictionary[int(i[5])] = (0, i[1], i[3])
+            elif j == 12: 
+                block_dictionary[int(i[5])] = (1, i[1], i[3])
+            elif j == 13:
+                block_dictionary[int(i[5])] = (2, i[1], i[3])
+            elif j == 14:
+                block_dictionary[int(i[5])] = (3, i[1], i[3])
+            
 
+    for i in block_printed_set:
+        if block_dictionary[i][0] == 0:
+            print(f"DUPLICATE BLOCK {i} IN INODE {block_dictionary[i][1]} AT OFFSET {block_dictionary[i][2]}")
+        elif block_dictionary[i][0] == 1:
+            print(f"DUPLICATE INDIRECT BLOCK {i} IN INODE {block_dictionary[i][1]} AT OFFSET {block_dictionary[i][2]}")
+        elif block_dictionary[i][0] == 2:
+            print(f"DUPLICATE DOUBLE BLOCK {i} IN INODE {block_dictionary[i][1]} AT OFFSET {block_dictionary[i][2]}")
+        elif block_dictionary[i][0] == 3:
+            print(f"DUPLICATE TRIPLE BLOCK {i} IN INODE {block_dictionary[i][1]} AT OFFSET {block_dictionary[i][2]}")
 
     return;
 
@@ -91,12 +144,9 @@ def main():
     inodes_per_group = int(linesdict['SUPERBLOCK'][0][6])
     global reserved_boundary
     reserved_boundary = int(inodes_per_group/inodes_per_block) + int(linesdict['GROUP'][0][8])
-    print(f"Reserved boundary is {reserved_boundary}")
 
     generate_free_list()
     scan_blocks()
-    print(block_allocated_set)
-    print(block_free_list_set)
     check_unreferenced_blocks()
     
 if __name__ == "__main__":
